39862b72722812a39072a08de8e9d58c51bffdbd
yondaime966369 <156741863+yondaime966369@users.noreply.github.com>
2024-04-16 14:27:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
- 
+ // Get the "Hire Me" button element
+const hireMeButton = document.querySelector('.hire__me');
+
+// Add a click event listener to the "Hire Me" button
+hireMeButton.addEventListener('click', () => {
+  // Display an alert message when the "Hire Me" button is clicked
+  alert('Hire Me button clicked!');
+});
